87d33cb19cb0d169327bf069b4cfa3df2344081b
Nick <calabro.nick@gmail.com>
2015-04-17 22:34:17 -0400

    bug fix left click

--- data/widget/widget.js
+++ data/widget/widget.js
@@ -1,8 +1,8 @@
-this.addEventListener('click', function(event){
-	if(event.button == 0 && event.shiftkey == false)
-		self.port.emit('left-click');
+this.addEventListener('click', function(event) {
+  if(event.button == 0 && event.shiftKey == false)
+    self.port.emit('left-click');
 
-	if(event.button == 2 || (event.button == 0 && event.shiftkey == true))
-		self.port.emit('right-click');
-	event.preventDefault();
+  if(event.button == 2 || (event.button == 0 && event.shiftKey == true))
+    self.port.emit('right-click');
+    event.preventDefault();
 }, true);
